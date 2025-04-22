ee72ff80ab98993cfbc7ae2f1c28485daa5fc916
anxi0uz <yuzkov.a@yandex.ru>
2025-04-22 20:44:27 +0500

    wpf

--- library-client/library-client/obj/Debug/net9.0-windows/App.g.cs
+++ library-client/library-client/obj/Debug/net9.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace library_client {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace library_client {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.1.0")]
         public static void Main() {
             library_client.App app = new library_client.App();
             app.InitializeComponent();
